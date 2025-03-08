7fd24c4c96d0c5290889e4f09879b1c506d38d88
Abudi <abduselamt47@gmail.com>
2025-03-09 00:10:07 +0300

    Header updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center mx-auto max-w-6xl p-3'>
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>Sahand</span>
+                <span className='text-slate-700'>Estate</span>
+            </h1>
+
+            <form className='bg-slate-100 p-3 rounded-lg'>
+                <input type='text' placeholder='Search....' className='bg-transparent '/>
+            </form>
+            
+        </div>
+
+    </header>
   )
 }
