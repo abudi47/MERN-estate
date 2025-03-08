fe32eff34fc683e45aad5ee86b37ceb3d231927f
Abudi <abduselamt47@gmail.com>
2025-03-08 14:49:32 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='bg-blue-100 shadow-amber-200'>
+      <h1 className=''>sdf</h1>
+    </div>
+  )
+}
+
+export default App
