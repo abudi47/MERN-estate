946bce6a0eea23d11f206ecda82991fb3b89ed0f
Abudi <abduselamt47@gmail.com>
2025-03-09 11:42:00 +0300

    Headere

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import Signin from '../pages/Signin'
 
 export default function Header() {
   return (
@@ -9,9 +12,23 @@ export default function Header() {
                 <span className='text-slate-700'>Estate</span>
             </h1>
 
-            <form className='bg-slate-100 p-3 rounded-lg'>
-                <input type='text' placeholder='Search....' className='bg-transparent '/>
+            <form className='bg-slate-100 p-3  rounded-lg flex items-center '>
+                <input type='text' placeholder='Search....' className=' bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <FaSearch className='text-slate-600'/>
             </form>
+
+            <ul className='flex gap-4'>
+                <Link to={'/'}>
+                <li className='hidden text-slate-700 sm:inline hover:underline'>Home</li>
+                </Link>
+                <Link to={'/about'}>
+                <li className='hidden text-slate-700 sm:inline hover:underline'>About</li>
+                </Link>
+                
+                <Link to={'/sign-in'}>
+                <li className=' text-slate-700 hover:underline'>SignIn</li>
+                </Link>
+            </ul>
             
         </div>
 
