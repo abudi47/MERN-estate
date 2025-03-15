9e8f451065efa0ef46755efede25533706f160bd
Abudi <abduselamt47@gmail.com>
2025-03-15 17:14:53 +0300

    complete image uplload functionalty

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
+import { account, storage } from "../appWrite/appwriteConfig";
+import { Await } from "react-router-dom";
+
+
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [imageUrl, setImageUrl] = useState("");
+  console.log(file);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  }; 
+
+ 
+  const handleFileUpload = async (file) => {
+    if (!file) {
+      console.error("No file selected.");
+      return;
+    }
+  
+    try {
+      const fileId = `unique()`; // Generates a unique ID
+      const response = await storage.createFile("67d5712d002738777653", fileId, file);
+      console.log("Upload successful:", response);
+
+      const imageUrl = storage.getFilePreview("67d5712d002738777653", response.$id);
+      setImageUrl(imageUrl);
+      console.log(imageUrl)
+    } catch (error) {
+      console.error("Upload failed:", error);
+    }
+  };
+  
   return (
     <div>
       <div className="max-w-lg mx-auto p-3 mY-7">
         <h1 className="text-3xl font-semibold   text-center  mx-auto">
           Profile
         </h1>
-        <form className="flex mt-7 flex-col gap-4 ">
+        <form className="flex mt-7 flex-col gap-4 " onSubmit={handleFileUpload}>
+          <input
+            onChange={handleFileChange}
+            type="file"
+            ref={fileRef}
+            hidden
+            accept="image/*"
+          />
           <img
+            onClick={() => fileRef.current.click()}
             className="rounded-full h-30 w-30 self-center object-cover cursor-pointer"
-            src={currentUser.avatar}
+            src={imageUrl || currentUser.avatar}
             alt="profile"
           />
+          
+          
           <input
             type="text"
             placeholder="username"
