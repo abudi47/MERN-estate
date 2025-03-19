1d7e6be95a962103d0a9bd25825e4cb159528b84
Abudi <abduselamt47@gmail.com>
2025-03-19 09:56:30 +0300

    complete create listing page UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -6,7 +6,7 @@ export default function CreateListing() {
       <h1 className="text-3xl font-semibold text-center my-7 ">
         Create Listing
       </h1>
-      <form className="flex flex-col  sm:flex-row">
+      <form className="flex flex-col  sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1 ">
           <input
             className="p-3 bg-white border border-gray-200 rounded-lg "
@@ -109,6 +109,21 @@ export default function CreateListing() {
               The first image will be the cover (max 6)
             </span>
           </p>
+          <div className="flex gap-4">
+            <input
+              type="file"
+              className="p-3 border shadow border-gray-300 rounded w-full"
+              id="images"
+              multiple
+              accept="/images/*"
+            />
+            <button className="text-green-700 border rounded uppercase hover:shadow-lg disabled:opacity-80 border-green-700">
+              Upload
+            </button>
+          </div>
+          <button className="text-white p-3 my-2 uppercase rounded-lg  bg-slate-700 hover:opacity-95 disabled:opacity-80">
+            Create listing{" "}
+          </button>
         </div>
       </form>
     </main>
