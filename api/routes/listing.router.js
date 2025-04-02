eec63c9871fd770420050df1211b39e6409d6cd6
Abudi <abduselamt47@gmail.com>
2025-04-02 18:11:31 +0300

    create update listing api route

--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createListing, deleteListing } from "../controllers/listingController.js";
+import { createListing, deleteListing, updateListing } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post("/create", createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 
 
