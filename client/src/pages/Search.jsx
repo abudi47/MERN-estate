4a92e73e3fad8b5f7059e9d1915978edcfc849ca
Abudi <abduselamt47@gmail.com>
2025-04-16 12:38:51 +0300

    Add onChange and onSubmit functionality to the search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,48 +1,97 @@
-import React from "react";
-
+import { useState } from "react";
 export default function Search() {
+  const [sidebardata, setsidebardata] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const handleChange = (e) => {};
   return (
     <div className="flex flex-col md:flex-row md:min-h-screen">
       <div className="p-7 border-b-2 border-slate-300 shadow-slate-50 shadow-xl  md:border-r-2">
         <form className="flex flex-col gap-7">
           <div className="flex items-center gap-2">
-            <label className="whitespace-nowrap font-semibold">Search Term:</label>
+            <label className="whitespace-nowrap font-semibold">
+              Search Term:
+            </label>
             <input
               type="text"
               id="searchTerm"
               placeholder="Search..."
               className="border rounded-lg p-3 w-full bg-white border-amber-50 shadow-md shadow-slate-600 focus:outline-none "
+              value={sidebardata.searchTerm}
+              onChange={handleChange}
             />
           </div>
 
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="all" className="w-5" />
+              <input
+                checked={sidebardata.type === "all"}
+                onChange={handleChange}
+                type="checkbox"
+                id="all"
+                className="w-5"
+              />
               <span>Rent&sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" className="w-5" />
+              <input
+                type="checkbox"
+                checked={sidebardata.type === "rent"}
+                onChange={handleChange}
+                id="rent"
+                className="w-5"
+              />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="sell" className="w-5" />
+              <input
+                type="checkbox"
+                checked={sidebardata.type === "sell"}
+                id="sell"
+                className="w-5"
+              />
               <span>Sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                checked={sidebardata.offer}
+                onChange={handleChange}
+              />
               <span>Offer</span>
             </div>
           </div>
           <div className="flex gap-2  flex-wrap items-center">
             <label className="font-semibold">Amenities:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" className="w-5" />
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.furnished}
+              />
               <span>Furnished</span>
             </div>
 
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" className="w-5" />
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.parking}
+              />
               <span>Parking</span>
             </div>
           </div>
@@ -50,13 +99,15 @@ export default function Search() {
           <div className="flex items-center gap-2">
             <label className="whitespace-nowrap font-semibold">Sort :</label>
             <select
+              onChange={handleChange}
+              defaultValue={'created_at_desc'}
               id="sort_order"
               className="border rounded-lg p-2 w-full bg-white  border-amber-50 shadow-md shadow-slate-600 focus:outline-none "
             >
-              <option value="all">Price high to low</option>
-              <option value="all">Price low to high</option>
-              <option>Latest</option>
-              <option>Oldest</option>
+              <option value="regularPrice_desc">Price high to low</option>
+              <option value="regularPrice_asc">Price low to high</option>
+              <option value="createdAt_desc">Latest</option>
+              <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
 
@@ -67,7 +118,9 @@ export default function Search() {
       </div>
 
       <div>
-        <h1 className="text-3xl font-semibold text-slate-700 p-3 mt-5">Listing results:</h1>
+        <h1 className="text-3xl font-semibold text-slate-700 p-3 mt-5">
+          Listing results:
+        </h1>
       </div>
     </div>
   );
