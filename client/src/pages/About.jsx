60a78618086c63ad3d7f6e1fbe7f8c198741c2e0
Abudi <abduselamt47@gmail.com>
2025-04-20 08:12:39 +0300

    complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-        <h1>artd</h1>
+    <div className="p-6   py-20 px-4 max-w-6xl mx-auto">
+      <h2 className="text-3xl font-bold text-slate-700 mb-4">
+        About Abd E-Real Estate
+      </h2>
+      <p className="text-slate-700 mb-4">
+        At <span className="font-semibold">Abd E-Real Estate</span>, we believe
+        that finding the perfect property should be an exciting and stress-free
+        experience. Whether you're looking to buy, sell, or rent, our dedicated
+        team is here to guide you every step of the way. With a deep
+        understanding of the local market and a passion for real estate, we
+        strive to deliver exceptional service and unmatched value to our
+        clients.
+      </p>
+      <p className="text-slate-700 mb-4">
+        Founded on the principles of trust, transparency, and integrity,{" "}
+        <span className="font-semibold">Abd E-Real Estate</span> has quickly
+        become a name you can rely on. We specialize in residential and
+        commercial properties, offering tailored solutions that meet your unique
+        needs and goals. Our mission is to connect people with their dream
+        properties while building long-lasting relationships grounded in
+        satisfaction and success.
+      </p>
+      <p className="text-slate-700">
+        Let <span className="font-semibold">Abd E-Real Estate</span> help you
+        make informed decisions and turn your property dreams into reality.
+      </p>
     </div>
-  )
+  );
 }
