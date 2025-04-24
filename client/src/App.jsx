d7086e6e2923fee8d47710362005bcce65a4c3b5
Abudi <abduselamt47@gmail.com>
2025-04-24 09:38:30 +0300

    path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import Home from "./pages/Home";
-import Signin from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import SignUP from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
@@ -20,8 +20,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-up" element={<SignUP />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/search" element={<Search />} />
         <Route path="/listing/:lisId" element={<Listing />} />
